5780d9c81b2574afc0f13238d82739ede0680163
Twin-2 <davidwhit818@gmail.com>
2021-07-28 19:45:26 -0700

    finished card 1 of lab 13

--- server.js
+++ server.js
@@ -15,11 +15,7 @@ app.use(cors());
 
 //mongoDB set up
 const mongooseOptions = { useNewUrlParser: true, useUnifiedTopology: true }
-mongoose.connect('mongodb://localhost:27017/books-db-lab13-a', mongooseOptions);
-
-//seeded mongo data
-// let david = new User({ name: 'David', email: 'thebestdavid@gmail.com', books: [{ name: 'Wheel of Time', author: 'Robert Jordan and Brandon Sanderson', genre: 'fantasy', description: 'An epic adventure in a world on magic and monsters', status: 'owned' }, { name: 'Drive', author: 'Dan Pink', genre: 'self-improvement', description: 'Learn what motivates you and how to leverage that to be your best!', status: 'owned' }, { name: 'Mistborn', author: 'Brandon Sanderson', genre: 'fantasy', description: 'An epic adventure in a world of powerful magic', status: 'owned' }] })
-// david.save();
+mongoose.connect('mongodb://localhost:27017/books-db-lab13-c', mongooseOptions);
 
 
 //auth0 set up
@@ -43,6 +39,7 @@ app.get('/auth-test', (req, res) => {
       res.json({ 'token': token })
     }
   });
+  console.log('profile page')
 });
 app.get('/books', handleGetBooks);
 
@@ -54,22 +51,36 @@ app.post('/books', (req, res) => {
       let book = new BookSchema(bookInfo);
       book.save();
       user.books.push(book)
-      console.log(user)
       user.save()
         .then(user => res.json(user.books))
         .catch(err => console.error(err))
     })
+  console.log('books page')
 })
 app.use('*', errorHandler);
 
 //route functions
 function handleGetBooks(req, res) {
-  console.log('books req body', req.body)
-  User.find({})
-    .then(users => {
-      console.log(users)
-      res.json(users)
-    })
+  let emails = req.query.user;
+  let username = req.query.name;
+  console.log('email', emails)
+  console.log('name', username)
+  if (!User.find({ 'email': `${emails}` })) {
+    User.find({ 'email': `${emails}` })
+      .then(users => {
+        console.log('users line 74', users)
+        res.json(users)
+      })
+  } else {
+    username = new User({ "name": `${username}`, "email": `${emails}` })
+    username.save();
+    User.find({ 'email': `${emails}` })
+      .then(users => {
+        console.log('users line 82', users)
+        res.json(users)
+      })
+  }
+  console.log('get books')
 }
 function errorHandler(req, res) {
   res.status(404).send('No Such Route')
